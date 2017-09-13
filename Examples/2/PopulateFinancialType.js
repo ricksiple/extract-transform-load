d348bcd22028a3d3b70f471c1257ca9b10d0272c
Rick Siple <rick@sipleart.com>
2017-09-13 15:41:51 -0400

    Started Example2, but require Sqlite3Source component

---
+++ Examples/2/PopulateFinancialType.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+
+class PopulateFinancialType {
+
+  name() {
+    return 'Populate FinancialType';
+  }
+
+  run(next, db) {
+
+    db.serialize(() => {
+        var stmt = db.prepare('INSERT INTO FinancialType (code, name) VALUES ($code, $name);');
+        stmt.run({$code: 'P', $name: 'Portfolio'});
+        stmt.run({$code: 'B', $name: 'Benchmark'});
+        stmt.finalize();
+    });
+
+  }
+
+}
+
+module.exports = PopulateFinancialType;
